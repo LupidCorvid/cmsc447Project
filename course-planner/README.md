4f79f770d693b9ac08c637812c9744790ff3eafe
ColeNa-cmsc <cna1@umbc.edu>
2025-02-25 15:25:16 -0500

    Adds drag and drop item to page.tsx
    I added dependencies, next.js and DnD-kit. I also added a drag and drop item to be played with.

--- course-planner/README.md
+++ course-planner/README.md
@@ -2,6 +2,8 @@ This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-
 
 ## Getting Started
 
+? Make sure directory is in course-planner ?
+
 First, run the development server:
 
 ```bash
--- course-planner/app/page.tsx
+++ course-planner/app/page.tsx
@@ -1,23 +1,47 @@
-import Image from "next/image";
+'use client';
+import React, { useState } from 'react';
+import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
 
-function App() {
-  return (
-    <html>
-      <body>
-        <div className="App">
-          <h1>Hello World!</h1>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+const DItem = () => {
+    const {attributes, listeners, setNodeRef, transform} = useDraggable(
+        {id: 'draggable-item',}
+    );
+    const style = {
+        transform: transform ? `translate(${transform.x}px, ${transform.y}px)` : '',
+        padding: '20px',
+        backgroundColor: 'lightblue',
+        cursor: 'grab',
+        width: '100px',
+        textAlign: 'center',
+    }
+    return (
+        <div ref={setNodeRef} {...listeners} {...attributes} style = {style}>
+            item
         </div>
-      </body>
-    </html>
-  );
+    )
 }
 
-export default App;
+function App() {
+    return (
+      <html>
+        <body>
+          <div className="App">
+            <h1>Hello World!</h1>
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read our docs
+            </a>
+          </div>
+            <DndContext>
+                <DItem/>
+            </DndContext>
+        </body>
+      </html>
+      
+    );
+  }
+  export default App;
