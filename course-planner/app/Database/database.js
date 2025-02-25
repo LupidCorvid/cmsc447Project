4d9c0008b663823fc27ab722f36e3b7e8112164e
LupidCorvid <lucidcanine921@gmail.com>
2025-02-24 19:18:55 -0500

    Added a new package for reading excel files
    Used command npm install read-excel-file --save

---
+++ course-planner/app/Database/database.js
@@ -0,0 +1,8 @@
+/*
+Planning:
+-Convert table data to JSON (https://oss.sheetjs.com/sheetjs/)
+    https://stackoverflow.com/questions/8238407/how-to-parse-excel-xls-file-in-javascript-html5
+    https://www.npmjs.com/package/read-excel-file -> npm install read-excel-file --save
+
+-Import table data every time on launch (of main server)
+*/
