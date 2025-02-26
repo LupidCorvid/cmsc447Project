93317507fa8fc0f3b321f0993f331764797bcbbe
RJWMD <113476572+RJWMD@users.noreply.github.com>
2025-02-25 20:08:04 -0500

    Dynamic list now can expand to a scrollbar

--- course-planner/app/searchbar/src/components/DynamicList.js
+++ course-planner/app/searchbar/src/components/DynamicList.js
@@ -1,14 +1,23 @@
+'use client';
+import React, {useState} from 'react'
+import {ScrollView} from 'react-dom'
+
 //Display elements in box
 const DynamicList = () =>
 {
     const elements = ["Component", "component2", "Comper3"];
 
     return(
-        <div>
+        <div style={{
+            borderStyle:'solid',
+            overflowY:'scroll',
+            height:'200px',
+            width:'150px'}}>
             {elements.map((element, i) => {
-                return <div>{element}</div>
+                return <div key={i}>{element}</div>
             })}
         </div>
+        
     );
 }
 //{elements.map((element, i) => React.createElement(element, {Key:i}))}
--- course-planner/app/searchbar/src/components/Searchbar.js
+++ course-planner/app/searchbar/src/components/Searchbar.js
@@ -4,22 +4,25 @@ import React, {useState} from 'react'
 
 const Searchbar = () =>
 {
-//Need to find data format for the table
-const [searchInput, setSearchInput] = useState("");
+    //Need to find data format for the table
+    const [searchInput, setSearchInput] = useState("");
 
-const filterChanged = (e) =>
-{
-    setSearchInput(e.value)
-}
+    const filterChanged = (e) =>
+    {
+        setSearchInput(e.value)
+    }
 
-return <div>
-<input
-type="text"
-placeholder = "Class name..."
-onChange={filterChanged}
-value={searchInput}>
-</input>
-</div>
+    
+    
+    return <div>
+    <input
+    type="text"
+    placeholder = "Class name..."
+    onChange={filterChanged}
+    value={searchInput}
+    style={{width:"150px"}}>
+    </input>
+    </div>
 };
 
 export default Searchbar;
