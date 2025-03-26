2cd3c84ab1aa6d2f0064c8ebfba32b7f908580f5
LupidCorvid <lucidcanine921@gmail.com>
2025-03-26 16:43:27 -0400

    Update page.tsx

--- course-planner/app/page.tsx
+++ course-planner/app/page.tsx
@@ -4,10 +4,58 @@ import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
 import DynamicList from "./searchbar/src/components/DynamicList";
 import Searchbar from "./searchbar/src/components/Searchbar"
 import Droppable from "./searchbar/src/components/DragAndDropTest";
+import styles from "./page.module.css";
 
 function Planner(){
   return(
+    <html>
+        <body>
+              <div id="Planner" style={{float: 'left'}}>
 
+                <h1 className={styles.headerStyle} style={{float:'left'}}>
+                  My Planner
+                </h1>
+
+                <p id="Text Under Planner" className={styles.textStyle} style={{clear:'both', float:'left', textAlign:'left', lineHeight: 1.7}}>
+                  Major: &nbsp;
+
+                  <select id="Update Major Dropdown" value={userMajor} onChange={event => UpdateMajor(event)} className={styles.majorDecideStyle}>
+                    <option value={"Undecided"}>Undecided</option>
+                    <option value={"Computer Science"}>Computer Science</option>
+                    <option value={"Computer Engineering"}>Computer Engineering</option>
+                    <option value={"Information Systems"}>Information Systems</option>
+                  </select>
+
+                  <br/>
+
+                  Recommended Credits per Semester: {recCredits}
+                </p>
+
+                <div id="Planner Dynamic List" className={styles.plannerStyle} style={{clear:'both', float: 'left', borderStyle: 'solid'}}>
+                  <DynamicList elements={tester} listId="1"/>
+                </div>
+
+                <div id="Notifications"  style={{clear:"left", lineHeight: .1}}>
+                  <p className={styles.notificationStyle}>*The following courses in your planner do not meet prerequisite requirements:</p>
+                  <br/>
+                  <p className={styles.notificationStyle}>*This plan does not meet graduation requirements for {userMajor}</p>
+                </div>
+              </div>
+
+              <div id="Course Search" style={{float: 'right'}}>
+
+                <h1 className={styles.headerStyle} style={{float:'left', paddingBottom: '95px'}}>
+                  Course Search
+                </h1>
+
+                <div id="Course Search Dynamic List" className={styles.plannerStyle} style={{clear:'both', float: 'right', borderStyle: 'solid'}}>
+                  <div style={{padding: '15px'}}> <Searchbar/> </div>
+                  <DynamicList elements={tester2} listId="2"/>
+                </div>
+
+              </div>
+        </body>
+      </html>
   );
 }
 
