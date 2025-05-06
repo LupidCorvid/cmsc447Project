b65b886376022ef4ca10a29d5671efa28e346d86
LupidCorvid <lucidcanine921@gmail.com>
2025-05-06 01:01:22 -0400

    Most of the notes page formatting is done

--- course-planner/app/NotesPage.page.tsx
+++ course-planner/app/NotesPage.page.tsx
@@ -34,9 +34,8 @@ export function NotesMenu(input:NoteProps)
         input.callbackFunction();
     }
 
-   
-
-    return (<><div style={
+    return (<>
+    <div style={
         {display:"block",
             position:"absolute",
             top:0,
@@ -46,34 +45,43 @@ export function NotesMenu(input:NoteProps)
             color:'black',
             opacity: .5,
             zIndex:1,
-            backgroundColor: "#000000"
+            backgroundColor: "#000000",
+            height: '100vh',
+            width: '100vw'
         }}>
-
     </div>
     <div style={{
         opacity:1,
         backgroundColor: "#FFFFFF",
         position: "relative",
-        top: "10%",
+        top: "7%",
         bottom: "10%",
-        left: "25%",
+        left: "17%",
         right: "25%",
         zIndex:2,
         height: "80vh",
-        width: "50vw",
+        width: "75vw",
     }
     }>
-        
-        <h1>Make a note for CMSC 202</h1>
-        Note:
-        <textarea name='NoteContents' cols={20} rows={20} className={styles.noteTextbox} onChange={textChanged} value={noteText}></textarea><br/>
-        <button type="button" onClick={PostNote}>Post Note</button>
-        <button type="button" onClick={closeNotes}>Close</button>
-        {GetNotesForCourse("CMSC203").map((e:PublicNote) =>
-            <h1>{e.note}</h1>
-            )}
+        <div >
+            <button type="button" onClick={closeNotes}
+                    style={{float:'right', paddingTop:10, paddingRight:20, fontWeight: 'bold', fontSize: 20, backgroundColor:'transparent', 
+                            border:'none', cursor: 'pointer', outline: 'inherit', color:'rgb(119, 116, 116)'}}>X</button>
+        </div>
 
+        <h2 style={{paddingTop:10, paddingLeft:150, float: 'left'}}>Notes from Students</h2> 
+        <h2 style={{paddingTop:10, paddingRight:130, float: 'right'}}>Make a note for CMSC 202</h2>
+        <div className={styles.noteboxStyle} style={{clear:'both', float:'left'}}>{GetNotesForCourse("CMSC203").map((e:PublicNote) =>
+            <h1>{e.note}</h1>
+            )}</div>
         
+        <div style={{clear:'right', float:'right', paddingRight:90}}>
+            <div style={{}}>Note Contents:</div>
+            <textarea name='NoteContents' cols={60} rows={20} style={{resize:'none'}} 
+                      onChange={textChanged} value={noteText}></textarea><br/>
+            <button type="button" onClick={PostNote}>Post Note</button>
+            
+        </div>
     </div>
     </>);
 }
